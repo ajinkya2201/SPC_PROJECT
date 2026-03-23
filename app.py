c7784ab1578964e2b6fa8602564ec3e6b5ff6a11
Ajinkya Shinde <ajinkyashinde1706@gmail.com>
2026-03-23 11:36:34 +0530

    Registration Form

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+users = []  
+@app.route("/")
+def home():
+    return "Home Page"
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        name = request.form.get("name")
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        users.append({
+            "name": name,
+            
+            "username": username,
+            
+            "password": password
+        })
+
+        return redirect(url_for("home"))
+
+    return render_template("register.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
