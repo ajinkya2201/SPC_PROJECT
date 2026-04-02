00952a6b68bf98782b97077184f91359216e7a9f
Ajinkya Shinde <ajinkyashinde1706@gmail.com>
2026-04-02 10:32:10 +0530

    Database and User Table Created

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+# create Database and Tables
+
+def init_db():
+    conn = sqlite3.connect("photo.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    create table if not exists users(
+    id integer primary key autoincrement,
+    name text,
+    username text unique,
+    password text
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
 
 app = Flask(__name__)
 
@@ -13,8 +33,21 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        # check in database
-        # if correct → login user
+        conn = sqlite3.connect("photo.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT * FROM users WHERE username=? AND password=?",
+            (username, password)
+        )
+
+        user = cursor.fetchone()
+        conn.close()
+
+        if user:
+            return "Login Successful"
+        else:
+            return "Invalid credentials"
 
     return render_template("login.html")
 
@@ -25,13 +58,21 @@ def register():
         username = request.form.get("username")
         password = request.form.get("password")
 
-        users.append({
-            "name": name,
-            
-            "username": username,
-            
-            "password": password
-        })
+        conn = sqlite3.connect("photo.db")
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute(
+                "insert into users(name,username,password) values(?,?,?)",
+                (name,username,password)
+            )
+
+            conn.commit()
+        except sqlite3.IntegrityError:
+            return "Username already exsits"
+
+        conn.close()
+        
 
         return redirect(url_for("login"))
 
@@ -39,4 +80,5 @@ def register():
 
 
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True)
